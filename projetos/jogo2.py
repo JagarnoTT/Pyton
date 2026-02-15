90e572b651a3b52e918f5f31df50a94a860bc3d3
Ericles Roque <jagarnot1236@gmail.com>
2026-02-14 22:17:27 -0300

    alteração

--- projetos/jogo2.py
+++ projetos/jogo2.py
@@ -11,12 +11,25 @@ def relatorio(historico):
     
     print(f"Parabéns, você derrotou o Boss! Com um total de {historicoDano} ataques! Com seu maior dano sendo de {historicoMAximo} pontos!");
 
+def AtacarEspada():
+    print("Ataque com espada");
+    corteEspada = random.randint(20, 100);
+    return corteEspada; #Return está devolvendo o valor para quem chamar a funcao atacarEspada;
+
+def AtacarMachado():
+    print("Ataque com machado");
+    destruirMachado = random.randint(50,150);
+    return destruirMachado;
+
+ataque_aleatorio = [AtacarEspada, AtacarMachado];
+    
+
 print(f"Bem vindo ao segundo RPG Dev");
 print(f"Prepare-se para uma batalha épica contra um Boss mais poderoso!");
 print(f"Para sair do jogo digite 'sair' quando for solicitado a inserir um dano crítico.");
 print('_' * 50);
 
-boss = 250;
+boss = 2000; #HP do Boss;
 print(f"Você vai enfrentar um Boss com {boss} HP!");
 print('_' * 50);
 time.sleep(3);
@@ -25,7 +38,8 @@ historico = [];
 while boss > 0:
     time.sleep(2);
 
-    dano = random.randint(20, 100);
+    TrocadeArmas = random.choice(ataque_aleatorio);
+    dano = TrocadeArmas();
 
     if dano > 80:
         print(f"Você causou {dano} de dano ao Boss, agora você pode inserir um dano crítico! O dano causado será dobrado.");
