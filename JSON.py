59812598eceb9211d4cc481ebb21dd08760813da
Ericles Roque <jagarnot1236@gmail.com>
2026-03-31 20:14:17 -0300

    alteração

--- JSON.py
+++ JSON.py
@@ -13,6 +13,18 @@ try:
 
         print("Arquivo criado com sucesso!");
 
+        dados_fabrica["Equipamento_2"]["Status"] = "Manutenção";
+
+    with open("status_maquina.json", "w", encoding="utf-8") as arquivo:
+        json.dump(dados_fabrica, arquivo, ensure_ascii=False, indent=4);
+        print("Arquivo atualizado com sucesso!");
+
+    with open("status_maquina.json", "r", encoding="utf-8") as arquivo:
+        dados_fabrica = json.load(arquivo);
+        status_maquina = json.dumps(dados_fabrica, indent=4, ensure_ascii=False);
+
+        print(status_maquina);
+
 except FileNotFoundError:
     print("Ocorreu um erro ao tentar acessar o arquivo");
 except Exception as e:
---
+++ SQL/BancoDAdos.py
@@ -0,0 +1,5 @@
+import sqlite3;
+
+conexao = sqlite3.conect("automacao_fabrica.db");
+
+cursor = conexao.cursor();
